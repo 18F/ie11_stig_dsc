c03cd1226ec45bc18f8dee267c57bff7a3ce5fbd
Peter Burkholder <peter.burkholder@gsa.gov>
2016-11-09 11:32:41 -0500

    added DSC wrapper stuff

--- ie_stig_dsc.ps1
+++ ie_stig_dsc.ps1
@@ -1,150 +1,170 @@
-      Registry V-46515 {
+configuration IE11Stig
+{
+  param(
+  [string[]]$ComputerName,
+  )
 
-        Ensure      = "Present"
-        Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\InternetSettings\Zones\3"
-        ValueType = "Dword"
-        ValueName = '1607'
-        ValueData = 3
-      end
+  Node $ComputerName {
+    # Debug Needed to prevent caching of resources, Implemented by
+    # Set-DscLocalConfigurationManager -Path $Dsc -Verbose -Force -CimSession $cs
+    LocalConfigurationManager
+    {
+        DebugMode = 'ForceModuleImport'
+        RebootNodeIfNeeded = $true
+    }
 
-# STOP_DESCRIBE V-46515
-      Registry V-46995 {
+    Registry V-46515 {
 
-        Ensure      = "Present"
-        Key = "HKLM\Software\Policies\Microsoft\Internet Explorer\Main"
-        ValueType = "Dword"
-        ValueName = 'Isolation64Bit'
-        ValueData = 1
-      end
+      Ensure      = "Present"
+      Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\InternetSettings\Zones\3"
+      ValueType = "Dword"
+      ValueName = '1607'
+      ValueData = 3
+    end
 
-# STOP_DESCRIBE V-46995
-      Registry V-46997 {
+    Registry V-46609-1 {
+      Ensure      = "Present"
+      Key = "HKLM\Software\Policies\Microsoft\Internet Explorer\Control Panel"
+      ValueType = "Dword"
+      ValueName = "History"
+      ValueData = 1
+    }
 
-        Ensure      = "Present"
-        Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
-        ValueType = "Dword"
-        ValueName = '270C'
-        ValueData = 0
-      end
 
-# STOP_DESCRIBE V-46997
-      Registry V-46999 {
+    Registry V-46609-2 {
+      Ensure      = "Present"
+      Key = "HKLM\oftware\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Url History"
+      ValueType = "Dword"
+      ValueName = "DaysToKeep"
+      ValueData = 40
+    }
 
-        Ensure      = "Present"
-        Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1"
-        ValueType = "Dword"
-        ValueName = '270C'
-        ValueData = 0
-      end
+    Registry V-46995 {
 
-# STOP_DESCRIBE V-46999
-      Registry V-47003 {
+      Ensure      = "Present"
+      Key = "HKLM\Software\Policies\Microsoft\Internet Explorer\Main"
+      ValueType = "Dword"
+      ValueName = 'Isolation64Bit'
+      ValueData = 1
+    end
 
-        Ensure      = "Present"
-        Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\0"
-        ValueType = "Dword"
-        ValueName = '270C'
-        ValueData = 0
-      end
+    Registry V-46997 {
 
-# STOP_DESCRIBE V-47003
-      Registry V-47005 {
+      Ensure      = "Present"
+      Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+      ValueType = "Dword"
+      ValueName = '270C'
+      ValueData = 0
+    end
 
-        Ensure      = "Present"
-        Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4"
-        ValueType = "Dword"
-        ValueName = '270C'
-        ValueData = 0
-      end
+    Registry V-46999 {
 
-# STOP_DESCRIBE V-47005
-      Registry V-47009 {
+      Ensure      = "Present"
+      Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1"
+      ValueType = "Dword"
+      ValueName = '270C'
+      ValueData = 0
+    end
 
-        Ensure      = "Present"
-        Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
-        ValueType = "Dword"
-        ValueName = '270C'
-        ValueData = 0
-      end
+    Registry V-47003 {
 
-# STOP_DESCRIBE V-47009
-      Registry V-64711 {
+      Ensure      = "Present"
+      Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\0"
+      ValueType = "Dword"
+      ValueName = '270C'
+      ValueData = 0
+    end
 
-        Ensure      = "Present"
-        Key = "HKLM\Software\Policies\Microsoft\Internet Explorer\PhishingFilter"
-        ValueType = "Dword"
-        ValueName = 'PreventOverride'
-        ValueData = 1
-      end
+    Registry V-47005 {
 
-# STOP_DESCRIBE V-64711
-      Registry V-64713 {
+      Ensure      = "Present"
+      Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4"
+      ValueType = "Dword"
+      ValueName = '270C'
+      ValueData = 0
+    end
 
-        Ensure      = "Present"
-        Key = "HKLM\Software\Policies\Microsoft\Internet Explorer\PhishingFilter"
-        ValueType = "Dword"
-        ValueName = 'PreventOverrideAppRepUnknown'
-        ValueData = 1
-      end
+    Registry V-47009 {
 
-# STOP_DESCRIBE V-64713
-      Registry V-64715 {
+      Ensure      = "Present"
+      Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
+      ValueType = "Dword"
+      ValueName = '270C'
+      ValueData = 0
+    end
 
-        Ensure      = "Present"
-        Key = "HKLM\Software\Policies\Microsoft\Internet Explorer\Security\ActiveX"
-        ValueType = "Dword"
-        ValueName = 'BlockNonAdminActiveXInstall'
-        ValueData = 1
-      end
+    Registry V-64711 {
 
-# STOP_DESCRIBE V-64715
-      Registry V-64717 {
+      Ensure      = "Present"
+      Key = "HKLM\Software\Policies\Microsoft\Internet Explorer\PhishingFilter"
+      ValueType = "Dword"
+      ValueName = 'PreventOverride'
+      ValueData = 1
+    end
 
-        Ensure      = "Present"
-        Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings"
-        ValueType = "Dword"
-        ValueName = 'PreventIgnoreCertErrors'
-        ValueData = 1
-      end
+    Registry V-64713 {
 
-# STOP_DESCRIBE V-64717
-      Registry V-64719 {
+      Ensure      = "Present"
+      Key = "HKLM\Software\Policies\Microsoft\Internet Explorer\PhishingFilter"
+      ValueType = "Dword"
+      ValueName = 'PreventOverrideAppRepUnknown'
+      ValueData = 1
+    end
 
-        Ensure      = "Present"
-        Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
-        ValueType = "Dword"
-        ValueName = '2301'
-        ValueData = 0
-      end
+    Registry V-64715 {
 
-# STOP_DESCRIBE V-64719
-      Registry V-64721 {
+      Ensure      = "Present"
+      Key = "HKLM\Software\Policies\Microsoft\Internet Explorer\Security\ActiveX"
+      ValueType = "Dword"
+      ValueName = 'BlockNonAdminActiveXInstall'
+      ValueData = 1
+    end
 
-        Ensure      = "Present"
-        Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4"
-        ValueType = "Dword"
-        ValueName = '2301'
-        ValueData = 0
-      end
+    Registry V-64717 {
 
-# STOP_DESCRIBE V-64721
-      Registry V-64723 {
+      Ensure      = "Present"
+      Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings"
+      ValueType = "Dword"
+      ValueName = 'PreventIgnoreCertErrors'
+      ValueData = 1
+    end
 
-        Ensure      = "Present"
-        Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1"
-        ValueType = "Dword"
-        ValueName = '1201'
-        ValueData = 3
-      end
+    Registry V-64719 {
 
-# STOP_DESCRIBE V-64723
-      Registry V-64725 {
+      Ensure      = "Present"
+      Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+      ValueType = "Dword"
+      ValueName = '2301'
+      ValueData = 0
+    end
 
-        Ensure      = "Present"
-        Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
-        ValueType = "Dword"
-        ValueName = '1201'
-        ValueData = 3
-      end
+    Registry V-64721 {
 
-# STOP_DESCRIBE V-64725
+      Ensure      = "Present"
+      Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4"
+      ValueType = "Dword"
+      ValueName = '2301'
+      ValueData = 0
+    end
+
+    Registry V-64723 {
+
+      Ensure      = "Present"
+      Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1"
+      ValueType = "Dword"
+      ValueName = '1201'
+      ValueData = 3
+    end
+
+    Registry V-64725 {
+
+      Ensure      = "Present"
+      Key = "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
+      ValueType = "Dword"
+      ValueName = '1201'
+      ValueData = 3
+    end
+  } # Node
+} # configuration
+
+IE11Stig -ComputerName cazgdso914-d
